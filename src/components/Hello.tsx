ba11c6332d80cf7905f5f4b32c04a306d6109601
rageboom <rageboom@gmail.com>
2020-06-16 21:46:31 +0900

    codesandbox test
    test

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,9 +1,14 @@
 import * as React from "react";
 
 export interface HelloProps {
-    compiler: string;
-    framework: string;
+  compiler: string;
+  framework: string;
+  dirty: string;
 }
 
-export const Hello = (props: HelloProps) => 
-    <h1> Hello from { props.compiler } and { props.framework }! </h1>
+export const Hello = (props: HelloProps) => (
+  <h1>
+    {" "}
+    Hello from {props.compiler} and {props.framework} {props.dirty}!{" "}
+  </h1>
+);
