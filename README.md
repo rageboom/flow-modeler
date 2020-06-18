00e8635f10991adcb37989f3ba3bdecdc8e91a5f
mac88 <mac88@mac88ui-MacBook-Air.local>
2020-06-18 23:19:57 +0900

    add webpack-dev-server & modified README.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# flow-modeler
+## Workflow Modeler
+
+# Modeler 정의
+여러 Activity 객체를 연결 하여 하나의 Flow를 구성하는 WIZWIG 에디터
+각 Flow 정보는 JSON 데이터 형태의 모델로 표현한다.
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackDevServer = require('webpack-dev-server');
 
 module.exports = {
     mode: "production",
